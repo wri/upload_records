ebe5a436908a91b96f7e8895ac260020a471e06b
Thomas Maschler <tmaschler@wri.org>
2019-04-29 14:27:40 -0400

    error logging
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- upload_records/concatenate_record.py
+++ upload_records/concatenate_record.py
@@ -1,5 +1,5 @@
 from retrying import retry
-from upload_records.utils import get_api_token
+from upload_records.utils import get_api_token, get_logfile, get_logger
 from datetime import datetime
 import boto3
 import requests
@@ -7,6 +7,7 @@ import click
 import os
 import json
 import logging
+import time
 
 
 @click.command()
@@ -16,14 +17,18 @@ import logging
 @click.option("--filetype", default="json")
 def cli(dataset, bucket, prefix, filetype):
 
-    logger = _get_logger(dataset)
+    get_logger(get_logfile(dataset))
 
     for obj in _get_s3_records(bucket, prefix):
         s3_path = "https://{}.s3.amazonaws.com/{}".format(bucket, obj.key)
         filename, file_extension = os.path.splitext(s3_path)
         if file_extension == ".{}".format(filetype):
-            logger.info("upload " + s3_path)
+            _get_task_log(dataset)
+            logging.info("Upload " + s3_path)
             _concatenate_record(dataset, s3_path)
+            time.sleep(2) #give API some time to update status
+
+    _get_task_log(dataset)
 
 
 def _get_logger(dataset):
@@ -42,6 +47,7 @@ def _get_logger(dataset):
 
     return logger
 
+
 def _get_s3_records(bucket_name, prefix):
 
     s3 = boto3.resource("s3")
@@ -50,10 +56,44 @@ def _get_s3_records(bucket_name, prefix):
     return bucket.objects.filter(Prefix=prefix)
 
 
+def _get_record_count(dataset):
+    url = "https://production-api.globalforestwatch.org/v1/query/{}?sql=select count(*) from table".format(dataset)
+    r = requests.get(url)
+    r_json = json.loads(r.text)
+    count = r_json["data"][0]["COUNT(*)"]
+    logging.info("Number of records: {}".format(count))
+
+
+@retry(wait_fixed=2000)
+def _get_task_log(dataset):
+
+    url = "https://production-api.globalforestwatch.org/v1/dataset/{}".format(dataset)
+    r = requests.get(url)
+    r_json = json.loads(r.text)
+    status = r_json["data"]["attributes"]["status"]
+
+    if status == "saved":
+        _get_record_count(dataset)
+        task_id = r_json["data"]["attributes"]["taskId"]
+        url = os.path.join("https://production-api.globalforestwatch.org" + task_id)
+        r = requests.get(url)
+        r_json = json.loads(r.text)
+        logs = r_json["data"]["attributes"]["logs"]
+        for log in logs:
+            if "withErrors" in log.keys() and log["withErrors"]:
+                logging.error(r_json["data"]["attributes"]["message"]["fileUrl"] + ": " + log["detail"])
+            if "error" in log.keys():
+                logging.error(r_json["data"]["attributes"]["message"]["fileUrl"] + ": " + log["error"])
+
+    else:
+        raise Exception("Dataset not saved")
+
+
 @retry(wait_fixed=2000)
 def _concatenate_record(dataset, record):
 
-    click.echo("Concatenate record " + record)
+    #_get_task_log(dataset)
+    logging.debug("Concatenate record " + record)
 
     url = "https://production-api.globalforestwatch.org/v1/dataset/{}/concat".format(dataset)
     token = json.loads(get_api_token())["token"]
--- upload_records/utils.py
+++ upload_records/utils.py
@@ -1,4 +1,8 @@
 import boto3
+import logging
+import sys
+from datetime import datetime
+import os
 
 
 def get_api_token():
@@ -7,3 +11,49 @@ def get_api_token():
         SecretId="gfw-api/prod-token"
     )
     return response["SecretString"]
+
+
+def get_logfile(dataset):
+    now = datetime.now()
+    dirname = os.path.dirname(__file__)
+    log_dir = os.path.join(dirname, "log")
+
+    try:
+        os.makedirs(log_dir)
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    logfile = "{}/{}_{}.log".format(log_dir, dataset, datetime.strftime(now, "%Y%m%d-%H%M%S"))
+
+    return logfile
+
+
+def get_logger(logfile, debug=False):
+    """
+    Build logger
+    :param logfile: Location of logfile
+    :param debug: Set Log Level to Debug or Info
+    :return: logger
+    """
+
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    sh = logging.StreamHandler(sys.stdout)
+    fh = logging.FileHandler(logfile)
+
+    #sh.setLevel(logging.DEBUG)
+    fh.setLevel(logging.INFO)
+
+    sh.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
+    root.addHandler(sh)
+    root.addHandler(fh)
+
+    return root
